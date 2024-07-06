897f4704eb08ab545394b89002fc2216d0553997
Aman Kumar <107541415+amankumar1804@users.noreply.github.com>
2024-07-06 07:41:24 +0530

    Update Login.js

--- mern-app/frontend/src/pages/Login.js
+++ mern-app/frontend/src/pages/Login.js
@@ -28,7 +28,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/login`;
+            const url = `https://assignment-rust-alpha.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
