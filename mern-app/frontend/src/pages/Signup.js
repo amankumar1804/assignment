cd0e0efac86cdd3c581593205359c64a5b2df1be
Aman Kumar <107541415+amankumar1804@users.noreply.github.com>
2024-07-06 07:42:13 +0530

    Update Signup.js

--- mern-app/frontend/src/pages/Signup.js
+++ mern-app/frontend/src/pages/Signup.js
@@ -31,7 +31,8 @@ function Signup() {
         }
 
         try {
-            const url = `http://localhost:8080/auth/signup`;
+            const url = `https://assignment-rust-alpha.vercel.app/auth/signup`;
+            
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
